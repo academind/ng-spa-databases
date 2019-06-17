a2a6f2d5bcde8fd59270adccc8ab3fcb9323970a
Maximilian Schwarzmüller <company@academind.com>
2019-06-17 07:47:42 +0200

    prepared code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Todo {
+  title: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-database';
+  todos: Todo[] = [];
+
+  onAddTodo(form: NgForm) {
+    this.todos.push({ title: form.value.todo });
+    form.resetForm();
+  }
+
+  onRemoveTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
 }
